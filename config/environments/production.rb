85ca38562e96064c90c104aef6da1bd6c3c83324
rizwan <rizwan@Aspire-5742.(none)>
2013-04-27 23:51:32 +0500

    changes for mailer host for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 UserAuth::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'http://user-auth.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
